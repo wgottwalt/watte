c698773020d84a3bf6eca500afab2375a2910da6
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-03-13 09:23:11 +0100

    started new watte version
    Started version 2 watte, which is a pure C version ment for static
    compiling (preferable with musl). Also updated readme.

--- README.md
+++ README.md
@@ -4,8 +4,19 @@ This is an attempt to write a very small editor with no ugly dependencis (well,
 except these libs/compilers which are part of nearly every Linux or BSD
 distribution), very small binary size and a small footprint.
 
+version1:
+
 The first working attempt is Watte and only depends on ncurses and a C++17
 compiler (can be changed quite easly to only need C++11).
 
 To compile it I usually run this:
-g++ -std=c++17 -W -Wall -Wextra -Os -s -o watte watte.cxx -lncurses
+g++ -std=c++17 -flto -W -Wall -Wextra -Os -s -o watte watte.cxx -lncurses
+
+version2:
+
+This version is version1 but completely done in C11. This is ment for static
+compiling. You should use lib musl for that to get some really impressive
+results (small size for a static binary).
+
+To compile it I usually run this:
+musl-gcc -std=c11 -flto -static -W -Wall -Wextra -Os -s -o watte watte.c -lncurses
---
+++ version2/watte.c
@@ -0,0 +1,152 @@
+/*
+ *  Watte - weird and trivially tiny editor
+ *  Copyright (C) 2020 Wilken 'Akiko' Gottwalt <akiko@linux-addicted.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define STRING_DEF_SIZE 128
+
+/*--- helper functions ---*/
+
+void __zero(void *src, const size_t size)
+{
+	char *ptr = src;
+
+	for (size_t i = 0; i < size; ++i)
+		ptr[i] = 0;
+}
+
+/*--- string structure and functions ---*/
+
+struct string {
+	size_t length;
+	size_t capacity;
+	char *data;
+};
+
+void string_destroy(struct string *str)
+{
+	if (str) {
+		if (str->data) {
+			free(str->data);
+			str->data = NULL;
+		}
+		free(str);
+		str = NULL;
+	}
+}
+
+void string_init(struct string *str)
+{
+	string_destroy(str);
+	str = (struct string *)malloc(sizeof (struct string));
+	if (str) {
+		str->data = (char *)malloc(STRING_DEF_SIZE);
+		if (str->data) {
+			str->length = 0;
+			str->capacity = STRING_DEF_SIZE;
+			__zero(str->data, STRING_DEF_SIZE);
+		} else
+			str->capacity = 0;
+	}
+}
+
+void string_init_from(struct string *str, const char *src)
+{
+	const size_t size = strlen(src) + 1;
+
+	if (str) {
+		if (str->capacity >= size) {
+			strncpy(str->data, src, size - 1);
+			__zero(str->data + size - 1, str->capacity);
+		} else {
+			str->data = (char *)realloc(str->data, size);
+			if (str->data) {
+				str->length = size - 1;
+				str->capacity = size;
+			} else {
+				str->length = 0;
+				str->capacity = 0;
+			}
+		}
+	} else {
+		str = (struct string *)malloc(size);
+		if (str) {
+			strncpy(str->data, src, size - 1);
+			str->length = size - 1;
+			str->capacity = size;
+		}
+	}
+}
+
+void string_clear(struct string *str)
+{
+	if (str && str->data) {
+		__zero(str->data, str->capacity);
+		str->length = 0;
+	}
+}
+
+/*--- stringlist structure and functions ---*/
+
+struct stringlist {
+};
+
+/*--- editor structure and functions ---*/
+
+struct editor {
+	struct stringlist *data;
+	struct string *filename;
+	int32_t xpos;
+	int32_t ypos;
+	int32_t sline;
+	bool running;
+};
+
+int32_t editor_run(struct editor *ed, const char *filename)
+{
+	string_init_from(ed->filename, filename);
+
+	while (ed->running)
+	{
+		printf("waiting 10 seconds for now:");
+		fflush(stdout);
+		for (int32_t i = 9; i >= 0; --i) {
+			sleep(1);
+			printf(" %d", i);
+			fflush(stdout);
+		}
+		printf("\n");
+		ed->running = false;
+	}
+
+	string_destroy(ed->filename);
+
+	return 0;
+}
+
+int32_t main(int32_t argc, char **argv)
+{
+	struct editor ed = {NULL, NULL, 0, 0, 0, true};
+
+	return editor_run(&ed, (argc == 2) ? argv[1] : "noname.txt");
+}
